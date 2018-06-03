4a76ded42c912d281bec190fd9202027c1e62f77
cuiqiongyu <18720956785@163.com>
2018-06-03 18:28:27 +0800

    微信小程序后台管理项目搭建

---
+++ src/main/java/com/thinkgem/jeesite/common/filter/PageCachingFilter.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright &copy; 2012-2016 <a href="https://github.com/thinkgem/jeesite">JeeSite</a> All rights reserved.
+ */
+package com.thinkgem.jeesite.common.filter;
+
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.constructs.web.filter.SimplePageCachingFilter;
+
+import com.thinkgem.jeesite.common.utils.SpringContextHolder;
+
+/**
+ * 页面高速缓存过滤器
+ * @author ThinkGem
+ * @version 2013-8-5
+ */
+public class PageCachingFilter extends SimplePageCachingFilter {
+
+	private CacheManager cacheManager = SpringContextHolder.getBean(CacheManager.class);
+	
+	@Override
+	protected CacheManager getCacheManager() {
+		return cacheManager;
+	}
+	
+}
